209b138d5911e756e13b990af1d76ec02eb763e7
Jubin Soni <jubinkumarsoni@gmail.com>
2020-06-01 22:38:44 +0530

    day1

--- day1/day_one.cpp
+++ day1/day_one.cpp
@@ -49,7 +49,7 @@ int main()
 	// using continue in loops
 	// it is used to skip an iteration if a particular condition is met
 	cout << "using continue in loops => ";
-	for(int i=0;i<size;i++)
+	for(int i =0;i<size;i++)
 	{
 		if(i == 2)
 		{
